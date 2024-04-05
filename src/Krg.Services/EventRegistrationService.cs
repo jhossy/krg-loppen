5b5ba266cb5658fb37f16016aee5d7e56cb50309
jhossy <hossy@godmail.dk>
2024-04-05 21:55:18 +0200

    added services

---
+++ src/Krg.Services/EventRegistrationService.cs
@@ -0,0 +1,61 @@
+﻿using Krg.Database;
+using Krg.Database.Models;
+using Krg.Domain;
+
+namespace Krg.Services
+{
+    public class EventRegistrationService : IEventRegistrationService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IEventRegistrationRepository _eventRegistrationRepository;
+
+        public EventRegistrationService(IUnitOfWork unitOfWork, IEventRegistrationRepository eventRegistrationRepository)
+        {
+            _unitOfWork = unitOfWork;
+			_eventRegistrationRepository = eventRegistrationRepository;
+        }
+
+		public async Task AddRegistration(Registration eventRegistration)
+		{
+			await _eventRegistrationRepository.AddRegistration(new EventRegistration
+			{
+                BringsTrailer = eventRegistration.BringsTrailer,
+                Department = eventRegistration.Department,
+                Email = eventRegistration.Email,
+                EventDate = eventRegistration.EventDate,
+                Name = eventRegistration.Name,
+                NoOfAdults = eventRegistration.NoOfAdults,
+                NoOfChildren = eventRegistration.NoOfChildren,
+                PhoneNo = eventRegistration.PhoneNo,
+                ShowName = eventRegistration.ShowName,
+            });
+
+			_unitOfWork.Commit();
+		}
+
+		public async Task<List<Registration>> GetRegistrations()
+		{
+			var registrations = await _eventRegistrationRepository.GetRegistrations();
+
+			return registrations.Select(reg => new Registration(reg)).ToList();
+		}
+
+		public void RemoveRegistration(Registration eventRegistration)
+		{
+			_eventRegistrationRepository.RemoveRegistration(new EventRegistration
+            {
+                BringsTrailer = eventRegistration.BringsTrailer,
+                Department = eventRegistration.Department,
+                Email = eventRegistration.Email,
+                EventDate = eventRegistration.EventDate,
+                Name = eventRegistration.Name,
+                NoOfAdults = eventRegistration.NoOfAdults,
+                NoOfChildren = eventRegistration.NoOfChildren,
+                PhoneNo = eventRegistration.PhoneNo,
+                ShowName = eventRegistration.ShowName,
+            });
+
+			_unitOfWork.Commit();
+		}
+	}
+}
---
+++ src/Krg.Services/Extensions/ServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Krg.Database;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Krg.Services.Extensions
+{
+    public static class ServicesExtensions
+	{
+		public static IServiceCollection AddServiceExtensions(this IServiceCollection services)
+		{
+			services.AddTransient<IUnitOfWork, UnitOfWork>();
+			services.AddTransient<IEventRegistrationRepository, EventRegistrationRepository>();
+			services.AddTransient<IEventRegistrationService, EventRegistrationService>();
+
+			return services;
+		}
+	}
+}
