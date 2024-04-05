9cbdbe6a62b9476f38432403f2fa3d35054fb567
jhossy <hossy@godmail.dk>
2024-04-05 21:55:52 +0200

    refactoring

---
+++ src/Krg.Services/IEventRegistrationService.cs
@@ -0,0 +1,13 @@
+﻿using Krg.Domain;
+
+namespace Krg.Services
+{
+	public interface IEventRegistrationService
+	{
+		Task AddRegistration(Registration eventRegistration);
+
+		void RemoveRegistration(Registration eventRegistration);
+
+		Task<List<Registration>> GetRegistrations();
+	}
+}
