bea4b401fdaa316b11141bc3ee115899e98633e4
jhossy <hossy@godmail.dk>
2024-05-03 17:12:13 +0200

    F/refactor event view (#14)
    * cleaned up registration form
    
    * Changed menu text from Home to Forside
    
    * fixed class typo
    
    * styled list
    
    * added dictionary items
    
    * Update Index.cshtml
    
    * updated styling
    
    * included logo
    
    * Update .gitignore
    
    * added ignored files
    
    * Update Krg.Web.csproj

--- src/Krg.Domain/Constants.cs
+++ src/Krg.Domain/Constants.cs
@@ -4,6 +4,8 @@
 	{
 		public const string NameRequiredMessage = "Navn skal angives";
 		public const string EmailRequiredMessage = "Email skal angives";
-		public const string DepartmentRequiredMessage = "Afdeling skal angives";
+		public const string DepartmentRequiredMessage = "Gren skal angives";
+		public const string NoOfAdultsRequiredMessage = "Antal voksne skal angives";
+		public const string NoOfChildrenRequiredMessage = "Antal børn skal angives";
 	}
 }
