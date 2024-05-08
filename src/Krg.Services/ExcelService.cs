0bfade231433cda8ba44161abb6382933fc44ea6
jhossy <hossy@godmail.dk>
2024-05-08 21:57:20 +0200

    added cols to excel

--- src/Krg.Services/ExcelService.cs
+++ src/Krg.Services/ExcelService.cs
@@ -16,26 +16,28 @@ namespace Krg.Web.Controllers
 
 				//Add the headers
 				worksheet.Cells[1, 1].Value = "Dato";
-				worksheet.Cells[1, 2].Value = "Navn";
-				worksheet.Cells[1, 3].Value = "Email";
-				worksheet.Cells[1, 4].Value = "Afdeling";
-				worksheet.Cells[1, 5].Value = "Antal voksne";
-				worksheet.Cells[1, 6].Value = "Antal børn";
-				worksheet.Cells[1, 7].Value = "Medbringer trailer";
-				worksheet.Cells[1, 8].Value = "Annulleret";
+				worksheet.Cells[1, 2].Value = "Senest opdateret";
+				worksheet.Cells[1, 3].Value = "Navn";
+				worksheet.Cells[1, 4].Value = "Email";
+				worksheet.Cells[1, 5].Value = "Afdeling";
+				worksheet.Cells[1, 6].Value = "Antal voksne";
+				worksheet.Cells[1, 7].Value = "Antal børn";
+				worksheet.Cells[1, 8].Value = "Medbringer trailer";
+				worksheet.Cells[1, 9].Value = "Annulleret";
 
 				//Add some items...
 				int i = 2;
 				foreach (var registration in registrations)
 				{
 					worksheet.Cells["A" + i].Value = registration.EventDate;
-					worksheet.Cells["B" + i].Value = registration.Name;
-					worksheet.Cells["C" + i].Value = registration.Email;
-					worksheet.Cells["D" + i].Value = registration.Department;
-					worksheet.Cells["E" + i].Value = registration.NoOfAdults;
-					worksheet.Cells["F" + i].Value = registration.NoOfChildren;
-					worksheet.Cells["G" + i].Value = registration.BringsTrailer;
-					worksheet.Cells["H" + i].Value = registration.IsCancelled;
+					worksheet.Cells["B" + i].Value = registration.UpdateDate;
+					worksheet.Cells["C" + i].Value = registration.Name;
+					worksheet.Cells["D" + i].Value = registration.Email;
+					worksheet.Cells["E" + i].Value = registration.Department;
+					worksheet.Cells["F" + i].Value = registration.NoOfAdults;
+					worksheet.Cells["G" + i].Value = registration.NoOfChildren;
+					worksheet.Cells["H" + i].Value = registration.BringsTrailer;
+					worksheet.Cells["I" + i].Value = registration.IsCancelled;
 
 					i++;
 				}
