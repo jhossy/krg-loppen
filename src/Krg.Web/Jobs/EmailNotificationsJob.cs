d170b80f47f4dbe74de3e96329d36408145188d4
jhossy <hossy@godmail.dk>
2024-05-15 22:20:42 +0200

    added retry

--- src/Krg.Web/Jobs/EmailNotificationsJob.cs
+++ src/Krg.Web/Jobs/EmailNotificationsJob.cs
@@ -1,5 +1,8 @@
 ﻿using Krg.Services.Interfaces;
+using Polly.Retry;
+using Polly;
 using Umbraco.Cms.Infrastructure.BackgroundJobs;
+using System.Threading;
 
 namespace Krg.Web.Jobs
 {
@@ -7,15 +10,15 @@ namespace Krg.Web.Jobs
     public class EmailNotificationsJob : IRecurringBackgroundJob
     {
         public TimeSpan Period => TimeSpan.FromMinutes(5);
-
         public TimeSpan Delay => TimeSpan.FromMinutes(3);
-
+        
         public event EventHandler PeriodChanged { add { } remove { } }
 
         private readonly IEmailservice _emailService;
         private readonly IEmailNotificationService _notificationService;
         private readonly ILogger<EmailNotificationsJob> _logger;
-
+        private readonly ResiliencePipeline _pipeline;
+        
         public EmailNotificationsJob(
             IEmailservice emailservice,
             IEmailNotificationService notificationService,
@@ -24,9 +27,26 @@ namespace Krg.Web.Jobs
             _emailService = emailservice;
             _notificationService = notificationService;
             _logger = logger;
-        }
 
-        public async Task RunJobAsync()
+			_pipeline = new ResiliencePipelineBuilder()
+	                                        .AddRetry(new RetryStrategyOptions() 
+                                            {
+												BackoffType = DelayBackoffType.Exponential,
+												Delay = TimeSpan.FromSeconds(3),
+												MaxRetryAttempts = 3,
+                                                UseJitter = true,
+                                                OnRetry = args =>
+                                                {
+                                                    _logger.LogWarning("OnRetry, Attempt: {0}", args.AttemptNumber);
+                                                    
+                                                    return default;
+												}
+                                            }) // Add retry using the default options
+	                                        .AddTimeout(TimeSpan.FromSeconds(10)) // Add 10 seconds timeout
+	                                        .Build(); // Builds the resilience pipeline
+		}
+
+		public async Task RunJobAsync()
         {
             Console.WriteLine("Executing EmailNotificationsJob...");
 
@@ -41,8 +61,11 @@ namespace Krg.Web.Jobs
             }
 
             foreach (var notification in nonProcessedNotifications)
-            {
-                await _emailService.SendEmail("support@spejderknud.dk", new[] { notification.To }, notification.Subject, notification.Body);
+			{
+                await _pipeline.ExecuteAsync(async token => 
+                {
+                    await _emailService.SendEmail("support@spejderknud.dk", new[] { notification.To }, notification.Subject, notification.Body);
+                });
 
                 _notificationService.RemoveNotification(notification.Id);
             }
