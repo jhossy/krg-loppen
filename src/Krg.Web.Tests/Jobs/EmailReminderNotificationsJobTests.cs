cc748d2d0271876436592ef3a3b8b0ca1ce2c8a9
jhossy <hossy@godmail.dk>
2024-06-08 20:53:16 +0200

    Ensured that reminder job will not run if any pending notifications (#40)

--- src/Krg.Web.Tests/Jobs/EmailReminderNotificationsJobTests.cs
+++ src/Krg.Web.Tests/Jobs/EmailReminderNotificationsJobTests.cs
@@ -32,6 +32,9 @@ namespace Krg.Web.Tests.Jobs
 		public async Task RunJobAsync_DoesNotTryToSendEmail_ProvidedZeroNotifications()
 		{
 			//Arrange
+			_mockNotificationService.Setup(service => service.GetNonProcessedNotifications())
+				.Returns(() => new List<Notification>());
+
 			_mockNotificationService.Setup(service => service.GetNonProcessedReminders())
 				.Returns(() => new List<EmailReminder>());
 
@@ -46,6 +49,9 @@ namespace Krg.Web.Tests.Jobs
         public async Task RunJobAsync_RetriesThreeTimes_ProvidedErrorOccurs()
         {
 			//Arrange
+			_mockNotificationService.Setup(service => service.GetNonProcessedNotifications())
+				.Returns(() => new List<Notification>());
+
 			_mockNotificationService.Setup(service => service.GetNonProcessedReminders())
 				.Returns(() => _fixture.Create<List<EmailReminder>>());
 
@@ -68,5 +74,25 @@ namespace Krg.Web.Tests.Jobs
 			//Assert
 			Assert.AreEqual(timesCalled, 3);
         }
-    }
+
+		[TestMethod]
+		public async Task RunJobAsync_DoesNotSendEmail_ProvidedPendingRegularNotifications()
+		{
+			//Arrange
+			_mockNotificationService.Setup(service => service.GetNonProcessedNotifications())
+				.Returns(() => _fixture.Create<List<Notification>>());
+
+			_mockNotificationService.Setup(service => service.GetNonProcessedReminders())
+				.Returns(() => _fixture.Create<List<EmailReminder>>());
+
+			int timesCalled = 0;
+			//_mockEmailService.Setup(emailService => emailService.SendEmail(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<string>(), It.IsAny<string>()));
+
+			//Act
+			await _sut.RunJobAsync();
+
+			//Assert
+			_mockEmailService.Verify(service => service.SendEmail(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+		}
+	}
 }
