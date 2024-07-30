32785b937429f84e7cb4d77b3543bb55a1007c95
jhossy <hossy@godmail.dk>
2024-07-30 21:11:57 +0200

    Added global exception handler

---
+++ src/Krg.Web/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Text;
+
+internal sealed class GlobalExceptionHandler : IExceptionHandler
+{
+	private readonly ILogger<GlobalExceptionHandler> _logger;
+
+	public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+	{
+		_logger = logger;
+	}
+
+	public async ValueTask<bool> TryHandleAsync(
+		HttpContext httpContext,
+		Exception exception,
+		CancellationToken cancellationToken)
+	{
+		_logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+		httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+		await httpContext.Response.WriteAsync(File.ReadAllText("./Views/Error/500.cshtml"), Encoding.GetEncoding("iso-8859-1"), cancellationToken);
+
+		return true;
+	}
+}
