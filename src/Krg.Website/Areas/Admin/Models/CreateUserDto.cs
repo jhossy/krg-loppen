76f0861209260c0aed9d6d819e135009bea8773f
jhossy <hossy@godmail.dk>
2025-01-12 14:44:09 +0100

    added email validation to user creation

--- src/Krg.Website/Areas/Admin/Models/CreateUserDto.cs
+++ src/Krg.Website/Areas/Admin/Models/CreateUserDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Krg.Website.Areas.Admin.Models;
 
 public class CreateUserDto
 {
+    [EmailAddress]
     public required string Email { get; set; }
 
     public required string Password { get; set; }
