0cdc14e2fd09f28361ca31969f79d7b9113f0288
jhossy <hossy@godmail.dk>
2025-01-20 21:25:46 +0100

    added translations for event pages

---
+++ src/Krg.Website/Models/Translations.cs
@@ -0,0 +1,32 @@
+namespace Krg.Website.Models;
+public struct Translations
+{
+    public struct Events
+    {
+        public const string EventDetailsHeader = "Detaljer";
+        
+        public const string EventOverview = "Oversigt";
+        
+        public const string DateLabel = "Dato";
+        
+        public const string NameLabel = "Navn";
+        
+        public const string PhoneLabel = "Telefon";
+        
+        public const string EmailLabel = "Email";
+
+        public const string SaveButtonText = "Gem";
+        
+        public const string CancelButtonText = "Annuller";
+        
+        public const string CreateButtonText = "Opret";
+        
+        public const string EditButtonText = "Rediger";
+        
+        public const string DeleteButtonText = "Slet";
+        
+        public const string SelectYear = "Vælg år";
+        
+        public const string AreYouSure = "Er du sikker på at du vil slette?";
+    }
+}
