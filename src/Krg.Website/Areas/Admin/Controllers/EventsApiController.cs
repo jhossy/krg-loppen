50997901c291358f2a1974580f72b1ec75f3646d
jhossy <hossy@godmail.dk>
2025-02-09 21:55:24 +0100

    F/tabs (#63)
    * made createEvent async
    
    * Update Index.cshtml
    
    * Update dotnet.yml

--- src/Krg.Website/Areas/Admin/Controllers/EventsApiController.cs
+++ src/Krg.Website/Areas/Admin/Controllers/EventsApiController.cs
@@ -1,6 +1,7 @@
 using Krg.Domain.Models;
 using Krg.Services.Interfaces;
 using Krg.Web.Extensions;
+using Krg.Website.Areas.Admin.Models;
 using Krg.Website.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,27 +11,60 @@ namespace Krg.Website.Areas.Admin.Controllers;
 
 [Area("Admin")]
 [Authorize]
-public class EventsApiController(IEventDateService eventDateService, IOptions<SiteSettings> siteSettings) : ControllerBase
+public class EventsApiController(IEventDateService eventDateService, IOptions<SiteSettings> siteSettings, ILogger<EventsApiController> logger) : ControllerBase
 {
     private readonly int[] _months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     private readonly SiteSettings _siteSettings = siteSettings.Value;
+   
+    [HttpPost]
+    public ActionResult CreateEvent([FromBody] CreateEventDto createEventDto)
+    {
+        if (DateTime.TryParse(createEventDto.Date, out DateTime dateTimeParsed))
+        {
+            var eventDate = eventDateService.GetEventByDate(dateTimeParsed);
+
+            if (eventDate != null)
+            {
+                return Ok(GetGroupedEventsByDate(dateTimeParsed.Year));
+            }
+
+            logger.LogInformation("Adding new event: {@createEventDto}", createEventDto);
+            
+            eventDateService.AddEventDate(new EventDate
+            {
+                Date = dateTimeParsed,
+                ContactName = createEventDto.ContactName,
+                ContactPhone = createEventDto.ContactPhoneNo,
+                ContactEmail = createEventDto.ContactEmail
+            });
+            
+            return Ok(GetGroupedEventsByDate(dateTimeParsed.Year));
+        }
+        
+        logger.LogError($"Error attempting to create event: {createEventDto}");
+        
+        return BadRequest("Error parsing date");
+    }
     
     [HttpGet]
     public IActionResult GetEvents(int year = 0)
+    {
+        return Ok(GetGroupedEventsByDate(year));
+    }
+
+    private Dictionary<string, List<EventDate>> GetGroupedEventsByDate(int year = 0)
     {
         if (year != DateTime.Now.Year && !_siteSettings.YearsToShow.Contains(year))
         {
             year = DateTime.Now.Year;
         }
-
+        
         var allDates = eventDateService
             .GetEvents(year)
             .OrderBy(x => x.Date)
             .ToList();
         
-        var sortedDates = GroupEventsByDate(allDates);
-        
-        return Ok(sortedDates);
+        return GroupEventsByDate(allDates);
     }
     
     private Dictionary<string, List<EventDate>> GroupEventsByDate(List<EventDate> allDates)
